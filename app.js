73d6677adde738a8532c92af93f582d0e791fe7b
Zhong Yucheng <674320899@qq.com>
2017-02-04 14:08:35 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var body = [
       '<body><div style="14px Helvetica Neue, Helvetica, Microsoft YaHei, Arial, 宋体, Hiragino Sans GB, STHeiti, WenQuanYi Micro Hei, SimSun, sans-serif">',                                                           
           '<div style="width:100%;pading:50px 100px;text-align:center;">',                                 
               '<img height="80" style="margin-top:100px;margin-left: -2%" src="https://dn-tenximage.qbox.me/logo.png" />',                                                                                            
-              '<h1>Hello world! - v7</h1>',                                                                      
+              '<h1>Hello world! - v8</h1>',                                                                      
               '<h2>This is CICD demo for Node.js application!</h2></div></div></body></html>'                                   
    ].join('');                                                                                             
                                                                                                            
